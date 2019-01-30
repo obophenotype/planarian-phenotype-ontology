665398540e793ba14ebd922d487c46513236ed43
Sofia Robb <sofia2341@gmail.com>
2019-01-30 13:50:04 -0700

    new release with eye,pigment,lesion and other phenotypes and patterns

--- src/patterns/getIDs.sh
+++ src/patterns/getIDs.sh
@@ -3,3 +3,9 @@ cut -f1 *tsv | grep  PLANP  | sort
 echo " "
 echo "***Checking for duplicate IDs"
 cut -f1 *tsv | grep  PLANP  | sort | uniq -c | sort -n | awk {'if($1 > 1){print $1" of "$2} '}
+
+OUT=`cut -f1 *tsv | grep  PLANP  | sort | uniq -c | sort -n | awk {'if($1 > 1){print $1" of "$2} '}`
+OUTLEN=$(echo -n $OUT | wc -c)
+if [ $OUTLEN -le 4 ] ;then
+   echo "No Duplicates"
+fi
