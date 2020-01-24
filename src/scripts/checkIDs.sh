e1ecd39696f9e2bea9da641b4b3d3d75aff25214
Sofia Robb <sofia2341@gmail.com>
2020-01-24 13:36:40 -0700

    aligned PLANP with updated PLANA

---
+++ src/scripts/checkIDs.sh
@@ -0,0 +1,13 @@
+echo "*** all ids"
+PREFIX=PLANP
+TSVs=../patterns/data/default/*tsv
+cut -f1 $TSVs | grep  $PREFIX  | sort  
+echo " "
+echo "***Checking for duplicate IDs"
+cut -f1 $TSVs | grep  $PREFIX | sort | uniq -c | sort -n | awk {'if($1 > 1){print $1" of "$2} '}
+
+OUT=`cut -f1 $TSVs | grep  $PREFIX  | sort | uniq -c | sort -n | awk {'if($1 > 1){print $1" of "$2} '}`
+OUTLEN=$(echo -n $OUT | wc -c)
+if [ $OUTLEN -le 4 ] ;then
+   echo "No Duplicates"
+fi
